7d0ceb36516a7b688540b75de116a0b30d0e2a5d
Ahmad Zaidi <zaidi@supplycart.my>
2020-04-08 03:48:27 +0800

    Release 1.2.0

--- src/datetime/components/index.js
+++ src/datetime/components/index.js
@@ -1,2 +1,3 @@
-export { default as DatePicker } from "./DatePicker";
-export { default as DateRangePicker } from "./DateRangePicker";
+export {default as DatePicker} from "./DatePicker";
+export {default as DateRangePicker} from "./DateRangePicker";
+export {default as DateTime} from "./Datetime";
