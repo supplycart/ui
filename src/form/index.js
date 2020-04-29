f6898721bfbefbb2d8cb8a575b3206228d4bd1d9
Ahmad Zaidi <zaidi@supplycart.my>
2020-04-29 15:34:48 +0800

    include FormLable in export

--- src/form/index.js
+++ src/form/index.js
@@ -1,5 +1,6 @@
 export {default as BaseInput} from "./components/BaseInput";
 export {default as Checkbox} from "./components/Checkbox";
+export {default as FormLabel} from "./components/FormLabel";
 export {default as PhoneInput} from "./components/PhoneInput";
 export {default as RemarksInput} from "./components/RemarksInput";
 export {default as TextField} from "./components/TextField";
