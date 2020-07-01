acfb26a38e7650e6b085c98efaa7face50b1fbd5
Ahmad Zaidi <zaidi@supplycart.my>
2020-07-01 16:24:11 +0800

    add class to paginate component

--- src/navigation/components/PerPageSelect.vue
+++ src/navigation/components/PerPageSelect.vue
@@ -1,20 +1,26 @@
 <template>
-    <div class="text-right ml-2">
-        <div class="inline-block dropdown relative">
-            <select
-                v-model="limit"
-                @change="$emit('change')"
-                class="selections w-auto"
-            >
-                <option value="30">Per Page 30</option>
-                <option value="50">Per Page 50</option>
-                <option value="100">Per Page 100</option>
-            </select>
-            <div class="pointer">
-                <FeatherIcon name="chevron-down"/>
-            </div>
-        </div>
+  <div class="perpage text-right ml-2">
+    <div class="inline-block dropdown relative">
+      <select
+        v-model="limit"
+        class="selections w-auto"
+        @change="$emit('change')"
+      >
+        <option value="30">
+          Per Page 30
+        </option>
+        <option value="50">
+          Per Page 50
+        </option>
+        <option value="100">
+          Per Page 100
+        </option>
+      </select>
+      <div class="pointer">
+        <FeatherIcon name="chevron-down" />
+      </div>
     </div>
+  </div>
 </template>
 <script>
     export default {
