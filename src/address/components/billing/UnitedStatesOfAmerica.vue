4c4b376bf337180f2f701f0536c92650e8894847
Fatimatul Zaharah <fatimatul@supplycart.my>
2021-09-09 12:03:03 +0800

    ST-7638 feat add new component for billing and delivery

---
+++ src/address/components/billing/UnitedStatesOfAmerica.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div>
+            <p>
+                <span v-if="showAttribute('entity_name')">{{
+                    value.entity_name
+                }}</span>
+
+                <span
+                    v-if="
+                        showAttribute('registration_no') &&
+                        value.registration_no
+                    "
+                    >&nbsp;({{ value.registration_no }})</span
+                >
+            </p>
+        </div>
+        <div>
+            <div v-if="this.display.length > 0">
+                <span v-if="showAttribute('pic_name')">
+                    {{ value.pic_name }}
+                </span>
+                <span v-if="showAttribute('pic_phone')">
+                    - {{ value.pic_phone }}
+                </span>
+            </div>
+            <p v-else>{{ value.pic_name }} - {{ value.pic_phone }}</p>
+        </div>
+        <div v-if="showAttribute('address')">
+            <p>
+                <span v-if="value.unit">{{ value.unit }}, </span>
+                <span v-if="value.floor">{{ value.floor }}, </span>
+                <span v-if="value.building">{{ value.building }}</span>
+            </p>
+            <p>{{ value.street }}</p>
+            <p>{{ value.city }}, {{ value.postcode }}</p>
+            <p>{{ value.state }}, {{ value.country }}</p>
+        </div>
+        <div v-if="showAttribute('einvoice_email')">
+            <p v-if="value.einvoice_email">
+                E-Invoice Mailbox: {{ value.einvoice_email }}
+            </p>
+        </div>
+        <div v-if="showAttribute('ref_no')">
+            <p v-if="value.ref_no">Billing ID: {{ value.ref_no }}</p>
+        </div>
+    </div>
+</template>
+<script>
+import AddressMixins from "../../mixins/address";
+export default {
+    name: "UnitedStatesOfAmericaBillingAddress",
+    mixins: [AddressMixins],
+};
+</script>
