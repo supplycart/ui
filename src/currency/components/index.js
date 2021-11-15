b76ac508b120602c080986cab5fd850d7ee64849
Fatimatul Zaharah <fatimatul@supplycart.my>
2021-11-15 17:46:55 +0800

    ST-8015 feat money input v2 component

--- src/currency/components/index.js
+++ src/currency/components/index.js
@@ -1,3 +1,4 @@
 export { default as Currency } from "./Currency";
 export { default as Money } from "./Money";
 export { default as MoneyInput } from "./MoneyInput";
+export { default as MoneyInputV2 } from "./MoneyInputV2";
