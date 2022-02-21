06cc2494e656a29b39b8b67870c708a97ab2f7be
Fatimatul Zaharah <fatimatul@supplycart.my>
2022-02-21 11:59:34 +0800

    ST-7648 feat cleanup code

--- src/address/index.js
+++ src/address/index.js
@@ -1,4 +1,2 @@
 export { default as Address } from "./components/Address";
 export { default as AddressForm } from "./components/AddressForm";
-export { default as AddressNew } from "./components/AddressNew";
-export { default as AddressFormNew } from "./components/AddressFormNew";
