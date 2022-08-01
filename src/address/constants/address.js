561019c5f4ea4b6c8f94875d9bb5031cb9e9a52a
kuanmy <kuan.mengyi@gmail.com>
2022-08-01 16:55:41 +0800

    ST-9217 FEAT Add tax registration no
    Remove address breaklines

--- src/address/constants/address.js
+++ src/address/constants/address.js
@@ -1,5 +1,7 @@
 const LABELS = {
     BILLING_ENTITY_CODE: "Billing Entity Code",
     E_INVOICE_MAILBOX: "E-Invoice Mailbox",
+    SALES_TAX_REGISTRATION_NO: "Sales Tax Registration No",
+    SERVICE_TAX_REGISTRATION_NO: "Service Tax Registration No",
 };
 export { LABELS };
